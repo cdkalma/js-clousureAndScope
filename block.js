dbdc46588dba110ada3b2ae1df674d0888cff03f
crdCalma <cdguttierrez11@gmail.com>
2024-03-19 09:33:23 -0500

    funcionamiento del blockScope

---
+++ block.js
@@ -0,0 +1,11 @@
+function fruits() {
+    if (true) {
+        var fruit1 = 'apple'
+        let fruit2 = 'kiwi' //block scope
+        const fruit3 = 'banana' //block scope
+
+        console.log(fruit2);
+        console.log(fruit3);
+    }
+    console.log(fruit1);
+}
